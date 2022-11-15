d3855440dc619b8f408ec7037544b0761e991ad7
Syed Ashar <syedashar25@gmail.com>
2022-11-15 17:12:23 -0600

    fetched local host data in react. Added the spring boot service as a proxy in package.json (Cors error)

--- bookstore_ui/src/App.js
+++ bookstore_ui/src/App.js
@@ -1,7 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState, useEffect} from "react";
+import axios from 'axios';
+
 
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect (() => {
+      getBooks();
+  }, [])
+
+  const getBooks  = () => {
+      axios.get(
+          '/books'
+      )
+      .then ((response) => {
+        setData(response.data)
+      })
+
+  }
+  console.log(data);
+
+
   return (
     <div className="App">
 
