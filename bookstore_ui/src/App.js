12ed8daddc87b3c24946dcef34d9c03755b3bf59
Syed Ashar <syedashar25@gmail.com>
2022-11-15 18:21:36 -0600

    Added MUI dependencies and MUI datagrid using the fetched data

--- bookstore_ui/src/App.js
+++ bookstore_ui/src/App.js
@@ -3,31 +3,93 @@ import './App.css';
 import {useState, useEffect} from "react";
 import axios from 'axios';
 
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import { DataGrid } from '@mui/x-data-grid';
 
-function App() {
-  const [data, setData] = useState([]);
+const columns = [
+    {   field: 'id',
+        headerName: 'ID',
+        width: 90
+    },
+    {
+        field: 'bookName',
+        headerName: 'Book Name',
+        width: 200,
+        editable: true,
+    },
+    {
+        field: 'buyerName',
+        headerName: 'Buyer Name',
+        width: 150,
+        editable: true,
+    },
+];
 
-  useEffect (() => {
-      getBooks();
-  }, [])
 
-  const getBooks  = () => {
-      axios.get(
-          '/books'
-      )
-      .then ((response) => {
-        setData(response.data)
-      })
+export default function App() {
+    const [data, setData] = useState([]);
 
-  }
-  console.log(data);
+    useEffect (() => {
+        getBooks();
+    }, [])
 
+    const getBooks  = () => {
+        axios.get(
+            '/books'
+        )
+        .then ((response) => {
+            setData(response.data)
+        })
 
-  return (
-    <div className="App">
+    }
+    console.log(data);
 
-    </div>
-  );
+    return (
+        <Box sx={{ height: 400, width: '100%' }}>
+            <DataGrid
+                rows={data}
+                columns={columns}
+                pageSize={5}
+                rowsPerPageOptions={[5]}
+                checkboxSelection
+                disableSelectionOnClick
+            />
+        </Box>
+    );
 }
 
-export default App;
+
+// function App() {
+//   const [data, setData] = useState([]);
+//
+//   useEffect (() => {
+//       getBooks();
+//   }, [])
+//
+//   const getBooks  = () => {
+//       axios.get(
+//           '/books'
+//       )
+//       .then ((response) => {
+//         setData(response.data)
+//       })
+//
+//   }
+//   console.log(data);
+//
+//
+//   return (
+//     <div className="App">
+//         {data.map(book => {
+//             return(
+//                 <table>
+//             ]
+//                 </table>
+//             )
+//         })}
+//     </div>
+//   );
+// }
+//
+// export default App;
